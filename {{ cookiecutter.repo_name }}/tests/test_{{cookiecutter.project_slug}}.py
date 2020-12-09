0d748741d722ff9ff0f090320b96e88a0a28b56d
Alicia <alicia.arenzana@gmail.com>
2020-12-09 11:59:27 +0100

    fixed tests

--- {{ cookiecutter.repo_name }}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{ cookiecutter.repo_name }}/tests/test_{{cookiecutter.project_slug}}.py
@@ -6,11 +6,9 @@
 import pytest
 {% if cookiecutter.command_line_interface|lower == 'click' %}
 from click.testing import CliRunner
-{% endif %}
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }} # noqa
-{% if cookiecutter.command_line_interface|lower == 'click' %}
 from {{ cookiecutter.project_slug }} import cli
 {% endif %}
+from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }} # noqa
 
 
 @pytest.fixture
@@ -26,7 +24,7 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
+{% if cookiecutter.command_line_interface|lower == 'click' %}
 
 
 def test_command_line_interface():
@@ -38,4 +36,4 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
-{%- endif %}
+{% endif %}
