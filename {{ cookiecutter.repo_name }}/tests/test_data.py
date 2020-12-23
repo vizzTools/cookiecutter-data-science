29127ff8ae8673fc0f9cf958adaf1cbc6fd392a3
elpamart <elena.palao@vizzuality.com>
2020-12-23 09:27:01 +0100

    add composite for generation of geojson features with hypothesis and an example test for geojson features

--- {{ cookiecutter.repo_name }}/tests/test_data.py
+++ {{ cookiecutter.repo_name }}/tests/test_data.py
@@ -10,6 +10,10 @@ follows common pandas convertions
 import pytest
 import pandas as pd
 import datatest as dt
+from hypothesis import given, example, assume
+import hypothesis.strategies as st
+from math import isnan #to remove nan from test
+from hypothesis_geojson import features
 
 
 @pytest.fixture(scope='module')
@@ -76,3 +80,110 @@ def test_float_types():
 if __name__ == '__main__':
     import sys
     sys.exit(pytest.main([__file__]))
+
+#test with hypothesis: https://hypothesis.readthedocs.io/en/latest/quickstart.html
+
+def encode(input_string):
+
+    if not input_string:
+        return []
+
+    count = 1
+    prev = ""
+    lst = []
+    for character in input_string:
+        if character != prev:
+            if prev:
+                entry = (prev, count)
+                lst.append(entry)
+            count = 1
+            prev = character
+        else:
+            count += 1
+    entry = (character, count)
+    lst.append(entry)
+    return lst
+
+
+def decode(lst):
+    q = ""
+    for character, count in lst:
+        q += character * count
+    return q
+
+@given(st.text()) #specifies how to provide the arguments
+@example("") #to check edge cases - we add here the test that we always want to perform
+def test_decode_inverts_encode(s):
+    assert decode(encode(s)) == s
+
+@given(st.integers(), st.integers())
+def test_ints_are_commutative(x, y):
+    assert x + y == y + x
+
+
+@given(x=st.integers(), y=st.integers())
+def test_ints_cancel(x, y):
+    assert (x + y) - y == x
+
+
+@given(st.lists(st.integers()))
+def test_reversing_twice_gives_same_list(xs):
+    # This will generate lists of arbitrary length (usually between 0 and
+    # 100 elements) whose elements are integers.
+    ys = list(xs)
+    ys.reverse()
+    ys.reverse()
+    assert xs == ys
+
+
+@given(st.tuples(st.booleans(), st.text()))
+def test_look_tuples_work_too(t):
+    # A tuple is generated as the one you provided, with the corresponding
+    # types in those positions.
+    assert len(t) == 2
+    assert isinstance(t[0], bool)
+    assert isinstance(t[1], str)
+
+@given(st.floats())
+def test_negation_is_self_inverse_for_non_nan(x):
+    assume(not isnan(x))
+    assert x == -(-x)
+
+
+
+
+@st.composite
+def distinct_strings_with_common_characters(draw):
+    x = draw(st.text(min_size=1))
+    y = draw(st.text(alphabet=x))
+    assume(x != y)
+    return (x, y)
+
+@given(s1=distinct_strings_with_common_characters(), s2= distinct_strings_with_common_characters()) #call the composite function
+def test_substraction(s1, s2):
+    print(s1, s2)
+
+#hypothesis for geojsons
+
+
+
+def find_name(feature):
+    """This function checks a geojson-like feature
+    for the `name` property.
+
+    This assumes that feature['properties']
+    is always a dictionary or mapping. But the GeoJSON spec
+    allows for `null` (becomes a python `None`) and causes this
+    to fail on otherwise valid geojson objects.
+
+    Would you think to test that? Hypothesis does.
+    """
+    # Get name property or default to empty string
+    return feature['properties'].get('name', '')
+
+
+# To use the hypothesis GeoJSON strategy,
+# decorate the test with the @given(feautures())
+@given(features())
+def test_find_name(feature):
+    find_name(feature)
